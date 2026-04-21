ca01b6e67a6b5e6a29b713fd78e455949af12fc4
Waynh77 <waynh77@gmail.com>
2026-04-21 18:30:37 +0700

    Remove unused Microsoft.OpenApi using directive
    The using Microsoft.OpenApi; statement was removed from Program.cs
    as it is no longer required or referenced in the codebase. This
    helps clean up unnecessary dependencies and improves code clarity.

--- PracticalBEsesi3/Program.cs
+++ PracticalBEsesi3/Program.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi;
 using Microsoft.OpenApi.Models;
 using PracticalBEsesi3.Data;
 using System.Text;
